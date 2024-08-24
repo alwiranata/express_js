d261d6837ed315950206abc0f9b79ef10ba84b63
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-24 17:34:17 +0700

    add __dirname

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from "express"
+import url from "url"
 const app = express()
 const port = 3000
 
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+
 app.get("/", (req, res) => {
-	res.json({
-		name: "Aldo",
-		old: 18,
-	})
+	// res.json({
+	// 	name: "Aldo",
+	// 	old: 18,
+	// })
+	res.sendFile("./page/index.html", {root: __dirname})
 })
 
 app.get("/home", (req, res) => {
