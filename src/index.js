e86cae9e840e4233e0e9073f2d7a3f9bf1d23c43
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-24 22:32:37 +0700

    update

--- src/index.js
+++ src/index.js
@@ -7,14 +7,25 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 
 app.get("/", (req, res) => {
 	// res.json({
-	// 	name: "Aldo",
+	// 	name:   "Aldo",
 	// 	old: 18,
 	// })
 	res.sendFile("./page/index.html", {root: __dirname})
 })
 
-app.get("/home", (req, res) => {
-	res.send("Page home")
+app.get("/about", (req, res) => {
+	res.sendFile("./page/about.html", {root: __dirname})
+})
+
+app.get("/contact", (req, res) => {
+	res.sendFile("./page/contact.html", {root: __dirname})
+})
+
+app.get("/items/:id", (req, res) => {
+	// console.log(req.params)
+	const id = req.params.id
+	const category = req.query.category
+	res.send("No items" + " " + id + "<br> and Category " + category)
 })
 
 app.use("*", (req, res) => {
