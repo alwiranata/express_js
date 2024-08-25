563de1185dc53182f03e597ca7ce9e1bbc261ef9
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-25 12:49:48 +0700

    add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
-import url from "url"
+// import url from "url"
+import router from "./router/index.js"
 const app = express()
 const port = 3000
 
+app.use(router)
 // const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 
 // app.get("/", (req, res) => {
@@ -39,8 +41,6 @@ const port = 3000
 // 	res.send("ini metode post barang")
 // })
 
-// app.get()
-
 // app.put("/barangs", (req, res) => {
 // 	res.send("ini metode put barangs")
 // })
@@ -71,9 +71,69 @@ const port = 3000
 // 	res.send("ini metode ab*cd")
 // })
 
-app.get("/abcd?(e)", (req, res) => {
-	res.send("ini metode get untuk ab(cd)?e")
-}) 
+// app.get("/abcd?(e)", (req, res) => {
+// 	res.send("ini metode get untuk ab(cd)?e")
+// })
+
+// app.get("/barangs", (req, res) => {
+// 	res.send("ini metode get barang")
+// })
+// app.get("/barangs/:id", (req, res) => {
+// 	res.send("ini metode get barang dengan id = " + req.params.id)
+// })
+
+// app.post("/barangs", (req, res) => {
+// 	res.send("ini metode post barang")
+// })
+
+// app.put("/barangs/:id", (req, res) => {
+// 	res.send("ini metode put barangs dengan id =" + req.params.id)
+// })
+
+// app.delete("/barangs/:id", (req, res) => {
+// 	res.send("ini metode delete barang dengan id =" + req.params.id)
+// })
+
+// const try1 = function (req, res, next) {
+// 	console.log("callback 1")
+// 	next()
+// }
+// const try2 = function (req, res, next) {
+// 	console.log("callback 2")
+// 	next()
+// }
+
+// const try3 = function (req, res, next) {
+// 	console.log("callback 3")
+// 	next()
+// }
+
+// app.get("/try", [try1, try2, try3], (req, res) => {
+// 	res.send("Try success running")
+// })
+
+// app
+// 	.route("/items")
+// 	.get((req, res) => {
+// 		res.send("ini metode untuk get items")
+// 	})
+// 	.put((req, res) => {
+// 		res.send("ini metode untuk put items")
+// 	})
+// 	.delete((req, res) => {
+// 		res.send("ini metode untuk delete items")
+// 	})
+// app
+// 	.route("/items/:id")
+// 	.get((req, res) => {
+// 		res.send("ini metode untuk get items dengan id = " + req.params.id)
+// 	})
+// 	.put((req, res) => {
+// 		res.send("ini metode untuk put items dengan id = " + req.params.id)
+// 	})
+// 	.delete((req, res) => {
+// 		res.send("ini metode untuk delete items dengan id = " + req.params.id)
+// 	})
 
 app.listen(port, () => {
 	console.log(`Server running to http://localhost:${port}`)
