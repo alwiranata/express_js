29b5b3f9e4751e3b36e1389f1d1147b837a23c14
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-31 10:16:50 +0700

    addRoutes

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ const port = 3000
 app.use(express.json()) // Mem-parsing data dalam format JSON
 
 app.get("/", (req, res) => {
+	res.send("Main Page")
+})
+
+app.get("/hello", (req, res) => {
+	console.log({getParams: req.query})
 	res.send("Hello World")
 })
 
-app.get("/main", (req, res) => {
-	res.send("Main Pages")
+app.post("/login", (req, res) => {
+	console.log({reqFromOutside: req.body})
+	if (req.body.name === "aldo" && req.body.password === "123") {
+		res.send("login suceess")
+	} else {
+		res.send("Name Failed")
+	}
 })
 
-app.post("/login", (req, res) => {
-	if (req.body.name === "aldo") {
-		console.log("login suceess")
-	}
+app.put("/username", (req, res) => {
+	console.log({updateData: req.body})
+	res.send("Update Success")
 })
 
 app.get("*", (req, res) => {
